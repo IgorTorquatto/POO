cb57ccee78d0c804cc894fbf90fb7a0bdc71641c
Igor <igor.torquato.20@gmail.com>
2023-03-27 11:31:54 -0300

    att arraylist

---
+++ ArrayList/src/teste_array2/Teste.java
@@ -0,0 +1,23 @@
+package teste_array2;
+
+import java.util.ArrayList;
+
+public class Teste {
+	public static void main(String[] args) {
+		/* 
+		 
+		 Tipo Primitivo -> Wrapper Class
+		 int -> Integer
+		 float -> Float
+		 double -> Double
+		 char -> Character
+		  
+		 */
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+		numeros.add(1);
+		
+		System.out.println(numeros.size());
+	}
+}
